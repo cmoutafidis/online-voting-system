d08060d24f1358947e1ed705116548c5118da781
Charis Moutafidis <charismoutafidis@gmail.com>
2020-10-28 13:58:56 +0200

    Added user fields

--- src/main/java/com/wannabeprogrammers/entity/User.java
+++ src/main/java/com/wannabeprogrammers/entity/User.java
@@ -1,6 +1,7 @@
 package com.wannabeprogrammers.entity;
 
 import javax.persistence.*;
+import java.security.PublicKey;
 
 @Entity
 @Table(name = "user")
@@ -9,6 +10,12 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private String username;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private PublicKey publicKey;
 
     public long getId() {
         return this.id;
@@ -16,5 +23,53 @@ public class User {
 
     public void setId(final long id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public PublicKey getPublicKey() {
+        return this.publicKey;
+    }
+
+    public void setPublicKey(final PublicKey publicKey) {
+        this.publicKey = publicKey;
     }
 }
